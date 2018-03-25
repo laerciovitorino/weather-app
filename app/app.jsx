6e8d93f95e1c8737aed202822397129a4b68058c
Laércio Vitorino <laerciovitorino@gmail.com>
2018-03-25 13:50:29 -0300

    About component setup

--- app/app.jsx
+++ app/app.jsx
@@ -4,10 +4,12 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var Main = require('Main');
 var Weather = require('Weather');
+var About = require('About');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Route path="about" component={About}/>
       <IndexRoute component={Weather}/>
     </Route>
   </Router>,
